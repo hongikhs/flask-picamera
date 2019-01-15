c94246448dd43c421450a59c4b7feeebbe305498
hongikhs <sw@hongik.hs.kr>
2019-01-15 14:03:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ html_stream = '<h1>드론 원격 카메라</h1><img src="/video_feed"><br><br>' 
 
 @app.route('/')                     # 웹브라우저가 http://주소/를 요청할 때 실행할 함수
 def home():
-    global html_still, html_record, html_list                           # 전역변수 연결
+    #global html_still, html_record, html_list                           # 전역변수 연결
     return html_stream + html_still + html_record + html_list           # 사진 촬영 버튼과 녹화 시작 버튼 및 촬영 목록을 제공
 
 @app.route('/still')                # 웹브라우저가 http://주소/still을 요청할 때 실행할 함수
 def still():
-    global f, html_still, html_record, html_list        # 전역 변수 연결
+    #global f, html_still, html_record, html_list        # 전역 변수 연결
+    global html_list
     f = str(datetime.datetime.now()) + '.jpg'           # 현재 날짜와 시간으로 파일 이름 생성
     cam.start_preview()                                 # 미리보기 시작
     cam.capture('./static/' + f)                        # 사진 촬영(static 디렉토리)
@@ -36,7 +37,8 @@ def still():
 
 @app.route('/record')               # 웹브라우저가 http://주소/record를 요청할 때 실행할 함수
 def record():
-    global f, html_stop, html_list, cam                 # 전역 변수 연결
+    #global f, html_stop, html_list, cam                 # 전역 변수 연결
+    global f
     f = str(datetime.datetime.now()) + '.h264'          # 현재 날짜와 시간으로 파일 이름 생성
     cam.start_preview()                                 # 미리보기 시작
     cam.start_recording('./static/' + f)                # 녹화 시작(static 디렉토리)
@@ -44,7 +46,8 @@ def record():
 
 @app.route('/stop')                 # 웹브라우저가 http://주소/stop을 요청할 때 실행할 함수
 def stop():
-    global f, html_still, html_record, html_list, cam   # 전역 변수 연결
+    #global f, html_still, html_record, html_list, cam   # 전역 변수 연결
+    global html_list
     cam.stop_recording()                                # 녹화 정지
     cam.stop_preview()                                  # 미리보기 종료
     html_list = '<br><a href="/static/' + f + '">' + f + '</a>' + html_list   # 촬영한 동영상 파일 링크를 목록에 추가
